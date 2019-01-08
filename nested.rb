b55b3d644aa2c2102ef1d38615a0145e6b66ce0c
Kelsey Low <kklowlow@gmail.com>
2019-01-08 04:44:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,9 +78,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash["yukihiro_matsumoto"] = {}
   programmer_hash[:yukihiro_matsumoto]["known_for"] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto]["languages"] = "LISP"
+  programmer_hash
 end
 
 def changing_alan
